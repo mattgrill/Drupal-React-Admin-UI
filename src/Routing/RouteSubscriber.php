ac4b2676c860606b20746439a6f101eaac56cc46
Ted Bowman <ted+git@tedbow.com>
2018-03-01 18:03:52 -0500

    refactor method to get routes to take over.

--- src/Routing/RouteSubscriber.php
+++ src/Routing/RouteSubscriber.php
@@ -17,9 +17,7 @@ class RouteSubscriber extends RouteSubscriberBase {
    */
   protected function alterRoutes(RouteCollection $collection) {
     // Routes the this module should take over that don't have callback routes.
-    $take_over_routes = [
-      'entity.user.collection',
-    ];
+    $take_over_routes = $this->getTakeOverRoutes();
     $call_back_routes = [];
     // Find all additional routes that this module should take over by checking
     // the '_admin_related_route' route option.
@@ -85,6 +83,24 @@ class RouteSubscriber extends RouteSubscriberBase {
     $json_route->setDefault('_controller', 'Drupal\drupal_admin_ui\Controller\DefaultController::getBlocks');
     $json_route->setRequirement('_format', 'json');
     return $json_route;
+  }
+
+  /**
+   * The names of the routes to take over.
+   *
+   * This only returns routes that don't have an associated callback route
+   * declared in drupal_admin_ui.routing.yml.
+   *
+   * @todo Move these to routing.yml.
+   *
+   * @return string[]
+   *   The route names.
+   */
+  protected function getTakeOverRoutes() {
+    $take_over_routes = [
+      'entity.user.collection',
+    ];
+    return $take_over_routes;
   }
 
 }
